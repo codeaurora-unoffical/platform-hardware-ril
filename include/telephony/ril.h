cf41b1d7342cc0e4f920e0371b77be37ff144d35
Satish Sugasi <c_ssugas@quicinc.com>
2010-02-24 15:45:20 -0800

    hardware/ril: Adds the functionality to remove the timer callback event.
    This provides option for qcril to delete the timed event setup  after
    getting response for manual/automatic network selection.
    
    CRs-fixed:226809

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -3451,6 +3451,13 @@ void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
 void RIL_requestTimedCallback (RIL_TimedCallback callback,
                                void *param, const struct timeval *relativeTime);
 
+/**
+ * Remove user-specified callback function
+ *  @param param  parameter list
+ */
+
+void RIL_removeTimedCallback(void *param);
+
 
 #endif /* RIL_SHLIB */
 
