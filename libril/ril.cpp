07dcf7003c413ba1bc9b527363124b50bd916496
Duy Truong <dtruong@codeaurora.org>
2013-01-16 13:20:19 -0800

    Update copyright to The Linux Foundation
    Change-Id: I30148abaa827d6b100d70ac73138df2215b5189c

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -1,7 +1,7 @@
 /* //device/libs/telephony/ril.cpp
 **
 ** Copyright 2006, The Android Open Source Project
-** Copyright (C) 2012, Code Aurora Forum. All rights reserved.
+** Copyright (C) 2012, The Linux Foundation. All rights reserved.
 ** Not a Contribution, Apache license notifications and license are retained
 ** for attribution purposes only.
 **
